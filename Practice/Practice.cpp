683961508c9464ce6e8fd30565b3d495691435db
Ana <dns80791@gmail.com>
2023-06-21 14:47:51 +0800

    Procents

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 int countLab=0;
 
+//void showMenu();
+
 struct student {
     string name;
     int* marks = new int[countLab];
     float points=0;
-    int percent = 0;
+    float percent = 0;
     bool autoMark=false;
     bool admission=true;
 };
@@ -23,6 +25,7 @@ int main()
     SetConsoleOutputCP(1251);
     int maxPoints = 0;    
     cout << "Введите количество лабораторных работ: "; cin >> countLab;
+    system("cls");
     int* weightLab = new int[countLab];
     for (int i = 0; i < countLab; i++) {
         cout << "Введите вес " << i + 1 << " лабораторной работы: "; cin >> weightLab[i];
@@ -34,6 +37,8 @@ int main()
     int countStudents;
     cout << "Введите количество студентов: "; cin >> countStudents;
     student* students = new student[countStudents];
+
+    bool isAutoMarks=false;
     
     for (int i = 0; i < countStudents; i++) {        
         cout << "Введите фамилию и имя " << i + 1 << " студента: "; cin.ignore(); getline(cin, students[i].name);
@@ -43,16 +48,22 @@ int main()
             if (students[i].admission == true && students[i].marks[j] <= 2)
                 students[i].admission = false;
         }
-        if (students[i].admission == true && students[i].points > 0.8 * maxPoints && students[i].points > maxStudPoints) 
-                   maxStudPoints = students[i].points;
+
+        if (students[i].points > maxStudPoints) {
+            maxStudPoints = students[i].points;
+            if (students[i].admission == true && students[i].points > 0.8 * maxPoints)
+                isAutoMarks = true;
+                
+        }
+        
          
         
     }
-    int procentForAutoMark=100;
+    float procentForAutoMark=80.1;
     for (int i = 0; i < countStudents; i++) {
             students[i].percent = (students[i].points / maxPoints) * 100;
 
-            if (maxStudPoints > 0) {
+            if (isAutoMarks) {
                 procentForAutoMark = 0.8 * (maxStudPoints / maxPoints * 100);
                 if (students[i].percent > procentForAutoMark && students[i].admission == true)
                     students[i].autoMark = true;
@@ -76,7 +87,7 @@ int main()
 
             cout << setw(7)<< students[i].marks[j];
         }
-        cout <<setw(8)<< students[i].points << setw(7) << students[i].percent << "%";
+        cout <<setw(8)<<fixed << setprecision(0)<< students[i].points << setw(7) <<fixed<<setprecision(1) << students[i].percent << "%";
 
         if (students[i].autoMark)
             cout <<setw(8) << "+";
@@ -96,8 +107,17 @@ int main()
         cout << setw(7) << weightLab[i];
     }
     cout << endl;
-    cout <<setw(20)<<"Максимальный балл" << setw(countLab * 9+2) << maxPoints<<endl;
-    cout << setw(20) << "Минимальный процент" << setw(countLab * 9 + 9) << procentForAutoMark<<"%";
-
+    cout <<setw(20)<<"Максимальный балл: "<<setw(7) << maxPoints << endl;
+    cout << setw(20) << "Наилучшая успеваемость:" << setw(7) << fixed << setprecision(0) << maxStudPoints<<endl;
+    cout << setw(20) << "Минимальный процент:" << setw(7) <<fixed<<setprecision(1) << procentForAutoMark<<"%";
+   
 }
+
+//void showMenu() {
+//    cout << setw(20) << " " << "Меню" << endl;
+//    cout << "0 - выйти" << endl;
+//    cout << "1 - добавить студента" << endl;
+//    cout << "2 - удалить студента" << endl;
+//    cout << "3 - вывести данные" << endl;
+//}
 
