31637b2bc71129b8336299bb2ac302da39ffa041
Ana <dns80791@gmail.com>
2023-06-22 23:33:07 +0800

    Input and output from\in file

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -4,69 +4,49 @@
 #include<fstream>
 #include<Windows.h>
 #include<vector>
-
 using namespace std;
-
-int countLab=0;
-
 
 struct student {
     string name;
-    int* marks = new int[countLab];
-    float points=0;
+    vector<int>marks; // массив для хранения оценок студента
+    int points=0;
     float percent = 0;
     bool autoMark=false;
     bool admission=true;
 };
 
-
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
-    
-    vector <student> students(0);
+    vector <student> students; //массив для данных о студентах
     int maxPoints = 0;
+    int countLab = 0;
+    int maxStudPoints = 0;
+    float minPercent = 80;
     bool isAutoMarks = false;
-
+    // выбор способа ввода
     bool vvod;
     cout << "Выберите способ ввода:" << endl << "0 - ввод из файла" << endl << "1 - ручной ввод" << endl;
     cin >> vvod;
-
-  
-    float maxStudPoints = 0;
-   
-    int* weightLab = new int[countLab];
+    vector<int> weightLab; // массив для хранения веса лаб
     if (!vvod) {
+        // ввод данных из файла
         ifstream fin("students.txt");
-        fin >> countLab;
-        
+        fin >> countLab; // ввод количества лаб
+        weightLab.resize(countLab); // расширене массива под заданное количество лаб
         for (int i = 0; i < countLab; i++) {
             fin >> weightLab[i];
-            maxPoints += 5 * weightLab[i];
-
-
+            maxPoints += 5 * weightLab[i]; // нахождение максимально возможного количества баллов
         }
         while (!fin.eof()) {
-
-            student readStudent;
-            readStudent.marks = new int[countLab];
-
+            student readStudent; // переменная для считывания данных о студенте
+            readStudent.marks.resize(countLab);// расширене массива под заданное количество лаб
             for (int i = 0; i < countLab; i++) {
-                fin >> readStudent.marks[i]; ;
-                readStudent.points += readStudent.marks[i] * weightLab[i];
-                if (readStudent.marks[i] <= 2)
-                    readStudent.admission = false;
+                fin >> readStudent.marks[i];
             }
-            readStudent.percent = (readStudent.points / maxPoints) * 100;
+            
 
-            if (readStudent.points > maxStudPoints) {
-                maxStudPoints = readStudent.points;
-                if (readStudent.admission == true && readStudent.points > 0.8 * maxPoints)
-                    isAutoMarks = true;
-
-            }
             getline(fin, readStudent.name);
 
             students.push_back(readStudent);
@@ -74,146 +54,129 @@ int main()
         }
 
         fin.close();
+    }
+    
+    else {
+        // ручной ввод
+        cout << "Введите количество лабораторных работ: "; cin >> countLab;
+        weightLab.resize(countLab);
 
-        float procentForAutoMark = 80.1;
-        for (int i = 0; i < students.size(); i++) {
-
-            if (isAutoMarks) {
-                procentForAutoMark = 0.8 * (maxStudPoints / maxPoints * 100);
-                if (students[i].percent > procentForAutoMark && students[i].admission == true)
-                    students[i].autoMark = true;
-            }
-
-        }
-
-        cout << setw(20) << " ";
+        cout << "Введите веса лабораторных работ через пробел: ";
         for (int i = 0; i < countLab; i++) {
-            cout << setw(7) << "Лаба " << i + 1;
+            cin >> weightLab[i];
+            maxPoints += 5 * weightLab[i];
         }
+
+        student readStudent;
+        bool addStudent;
+
+        do {
+            cout << "Введите имя и фамилию студента: "; cin.ignore(); getline(cin, readStudent.name);
+            cout << "Введите через пробел оценки студента за лабы: ";
+            readStudent.marks.resize(countLab);// расширене массива под заданное количество лаб
+            for (int i = 0; i < countLab; i++) {
+                cin >> readStudent.marks[i];
+            }
+            students.push_back(readStudent);
+            cout <<"Выберите дальнейшее действие:" << endl << "0 - добавить студента" << endl << "1 - вывести таблицу"<<endl;
+            cin >> addStudent;
+            
+        } while (!addStudent);
+    }
+
+    for (int i = 0; i < students.size(); i++) {
+        for (int j = 0; j < countLab; j++) {
+            students[i].points += students[i].marks[j]*weightLab[j];
+            if (students[i].admission && students[i].marks[j] <= 2)
+                students[i].admission = false;
+        }
+        students[i].percent = float(students[i].points)/ maxPoints*100;
+        if (students[i].admission && students[i].percent > 80 && students[i].points > maxStudPoints) {
+            maxStudPoints = students[i].points;
+            isAutoMarks = true;
+            minPercent = 0.8 * students[i].percent;
+        }
+
+        if (isAutoMarks) {
+            if (students[i].admission && students[i].percent > minPercent)
+                students[i].autoMark = true;
+        }
+    }
+
+    bool out;
+    cout << "Выберите способ вывода:" << endl<< "0 - вывод в консоль" << endl << "1 - вывод в файл" << endl;
+    cin >> out;
+    if (!out) {
+        cout << setw(20) << " ";
+        for (int i = 0; i < countLab; i++)
+            cout << setw(7) << "Лаба " << i + 1;
 
         cout << setw(6) << "Итог" << setw(9) << "Процент" << setw(9) << "Автомат" << setw(8) << "Допуск" << endl;
 
-        for (vector<student>::iterator it = students.begin(); it != students.end(); it++) {
-            cout << setw(20) << (*it).name << " ";
-            for (int i = 0; i < countLab; i++) {
-                cout << setw(7) << (*it).marks[i] << " ";
-            }
+        for (int i = 0; i < students.size(); i++) {
+            cout << setw(20) << students[i].name;
+            for (int j = 0; j < countLab; j++)
+                cout << setw(7) << students[i].marks[j];
+            cout << setw(10) << fixed << setprecision(0) << students[i].points << setw(8) << fixed << setprecision(1) << students[i].percent << "%";
 
-            cout << setw(4) << fixed << setprecision(0) << (*it).points << setw(8) << fixed << setprecision(1) << (*it).percent << "% ";
-
-            if ((*it).autoMark)
-                cout << setw(6) << "+";
+            if (students[i].autoMark)
+                cout << setw(8) << "+";
             else
-                cout << setw(6) << "-";
+                cout << setw(8) << "-";
 
-            if ((*it).admission)
-                cout << setw(9) << "+";
+            if (students[i].admission)
+                cout << setw(8) << "+";
             else
-                cout << setw(9) << "-";
+                cout << setw(8) << "-";
 
             cout << endl;
-       
-
         }
+
+        cout << endl;
+        cout << "Вес лабы: ";
+        for (int i = 0; i < countLab; i++)
+            cout << weightLab[i] << " ";
+        cout << endl;
+        cout << "Максимальный балл: " << maxPoints << endl << "Наилучшая успеваемость: " << fixed << setprecision(0) << maxStudPoints << endl;
+        cout << "Минимальный процент: " << fixed << setprecision(1) << minPercent << "%";
     }
-    else {            
-            cout << "Введите количество лабораторных работ: "; cin >> countLab;
-            int* weightLab = new int[countLab];
+    else {
+        ofstream fout("studentsResults.txt");
+        fout << setw(20) << " ";
+        for (int i = 0; i < countLab; i++)
+            fout << setw(7) << "Лаба " << i + 1;
 
-            for (int i = 0; i < countLab; i++) {
-                cout << "Введите вес " << i + 1 << " лабораторной работы: "; cin >> weightLab[i];
-                maxPoints += 5 * weightLab[i];
-            }
+        fout << setw(6) << "Итог" << setw(9) << "Процент" << setw(9) << "Автомат" << setw(8) << "Допуск" << endl;
 
-            maxStudPoints = 0;
-            int countStudents;
-            cout << "Введите количество студентов: "; cin >> countStudents;
-            student* students = new student[countStudents];
+        for (int i = 0; i < students.size(); i++) {
+            fout << setw(20) << students[i].name;
+            for (int j = 0; j < countLab; j++)
+                fout << setw(7) << students[i].marks[j];
+            fout << setw(10) << fixed << setprecision(0) << students[i].points << setw(8) << fixed << setprecision(1) << students[i].percent << "%";
 
+            if (students[i].autoMark)
+                fout << setw(8) << "+";
+            else
+                fout << setw(8) << "-";
 
+            if (students[i].admission)
+                fout << setw(8) << "+";
+            else
+                fout << setw(8) << "-";
 
-            for (int i = 0; i < countStudents; i++) {
-                cout << "Введите фамилию и имя " << i + 1 << " студента: "; cin.ignore(); getline(cin, students[i].name);
-                for (int j = 0; j < countLab; j++) {
-                    cout << "Введите оценку за " << j + 1 << " лабораторную работу: "; cin >> students[i].marks[j];
-                    students[i].points += students[i].marks[j] * weightLab[j];
-                    if (students[i].admission == true && students[i].marks[j] <= 2)
-                        students[i].admission = false;
-                }
+            fout << endl;
+        }
 
-                if (students[i].points > maxStudPoints) {
-                    maxStudPoints = students[i].points;
-                    if (students[i].admission == true && students[i].points > 0.8 * maxPoints)
-                        isAutoMarks = true;
+        fout << endl;
+        fout << "Вес лабы: ";
+        for (int i = 0; i < countLab; i++)
+            fout << weightLab[i] << " ";
+        fout << endl;
+        fout << "Максимальный балл: " << maxPoints << endl << "Наилучшая успеваемость: " << fixed << setprecision(0) << maxStudPoints << endl;
+        fout << "Минимальный процент: " << fixed << setprecision(1) << minPercent << "%";
 
-                }
-                students[i].percent = (students[i].points / maxPoints) * 100;
-
-                if (students[i].points > maxStudPoints) {
-                    maxStudPoints = students[i].points;
-                    if (students[i].admission == true && students[i].points > 0.8 * maxPoints)
-                        isAutoMarks = true;
-
-                }
-
-
-            }
-            float procentForAutoMark = 80.1;
-            for (int i = 0; i < countStudents; i++) {
-
-                if (isAutoMarks) {
-                    procentForAutoMark = 0.8 * (maxStudPoints / maxPoints * 100);
-                    if (students[i].percent > procentForAutoMark && students[i].admission == true)
-                        students[i].autoMark = true;
-                }
-
-            } 
-            cout << setw(20) << " ";
-            for (int i = 0; i < countLab; i++) {
-                cout << setw(7) << "Лаба " << i + 1;
-            }
-
-            cout << setw(6) << "Итог" << setw(9) << "Процент" << setw(9) << "Автомат" << setw(8) << "Допуск" << endl;
-
-            for (int i = 0; i < countStudents; i++) {
-
-
-                cout << setw(20) << students[i].name;
-
-                for (int j = 0; j < countLab; j++) {
-                    ;
-
-                    cout << setw(7) << students[i].marks[j];
-                }
-                cout << setw(8) << fixed << setprecision(0) << students[i].points << setw(7) << fixed << setprecision(1) << students[i].percent << "%";
-
-                if (students[i].autoMark)
-                    cout << setw(8) << "+";
-                else
-                    cout << setw(8) << "-";
-
-                if (students[i].admission)
-                    cout << setw(8) << "+";
-                else
-                    cout << setw(8) << "-";
-
-                cout << endl;
-            }
-                
-            cout << endl;
-
-            cout<< "Вес лабы";
-            for (int i = 0; i < countLab; i++) {
-                cout<< weightLab[i]<<" ";
-            }
-            cout << endl;
-            cout << "Максимальный балл: " << maxPoints << endl;
-            cout << "Наилучшая успеваемость: " << fixed << setprecision(0) << maxStudPoints << endl;
-            cout << "Минимальный процент: " << fixed << setprecision(1) << procentForAutoMark << "%";
-    }  
-
-    
-    
-
+        cout<<"Результаты успешно записаны в файл studentsResults.txt";
+    }
+       
 }
 
